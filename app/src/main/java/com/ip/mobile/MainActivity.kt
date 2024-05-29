8fffb2be91b5ff911da36b6110581d0e8b9ef43f
nishant4tech <nish4tech@gmail.com>
2024-05-29 18:02:39 -0400

    first commit

---
+++ app/src/main/java/com/ip/mobile/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.ip.mobile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.ip.mobile.ui.theme.IpMobileTheme
+import kotlinx.serialization.Serializable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            IpMobileTheme {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = ScreenA
+                ){
+                    composable<ScreenA> {
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Button(onClick = {
+                                navController.navigate(ScreenB(
+                                    name = null,
+                                    age = 10
+                                ))
+                            }) {
+                                Text(text = "Go to ScreenB")
+                            }
+                        }
+                    }
+                    composable<ScreenB> {
+                        val args = it.toRoute<ScreenB>()
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(text = "${args.name} is ${args.age} years old")
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+@Serializable
+object ScreenA
+
+@Serializable
+data class ScreenB(
+    val name: String?,
+    val age: Int
+)
+
