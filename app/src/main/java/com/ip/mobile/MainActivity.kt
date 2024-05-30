8bfa9191325a44c39a3fabc96b522ec1503531d9
nishant4tech <nish4tech@gmail.com>
2024-05-30 16:57:25 -0400

    rename package name

--- app/src/main/java/com/ip/mobile/MainActivity.kt
+++ app/src/main/java/com/ip/mobile/MainActivity.kt
@@ -27,7 +27,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.ip.mobile.feature_home.HomeNavHost
-import com.ip.mobile.feature_managefunds.ManageFundsNavHost
+import com.ip.mobile.feature_manage_funds.ManageFundsNavHost
 import com.ip.mobile.feature_settings.SettingsNavHost
 import com.ip.mobile.ui.theme.IpMobileTheme
 
